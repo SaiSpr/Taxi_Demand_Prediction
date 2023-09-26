faed5f56ac4ea56fde834136f6adb950cfcbfbf4
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-09-26 13:14:37 +0530

    Update frontend.py

--- src/frontend.py
+++ src/frontend.py
@@ -18,8 +18,8 @@ from src.plot import plot_one_sample
 st.set_page_config(layout="wide")
 
 # title
-# current_date = datetime.strptime('2023-01-05 12:00:00', '%Y-%m-%d %H:%M:%S')
-current_date = pd.to_datetime(datetime.utcnow()).floor('H')
+current_date = datetime.strptime('2023-09-20 10:59:59', '%Y-%m-%d %H:%M:%S')
+# current_date = pd.to_datetime(datetime.utcnow()).floor('H')
 st.title(f'Taxi demand prediction 🚕')
 st.header(f'{current_date} UTC')
 
